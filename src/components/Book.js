523384fba439352e7aa06a17c327702c695e6a57
Maria Knabe <maria@Charmander.local>
2020-06-13 22:17:33 -0500

    Add goodreads link

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,32 +5,34 @@ const Book = ({book}) => {
   if (!book) return null;
   return (
       <div className="Book">
-        <Card
-            size="small"
-            hoverable
-            cover={
-              <img src={book.image} alt="Book cover"/>
-            }
-        >
-          <Card.Meta
-              title={
-                  <div>
-                      <span className="Book-icon">
-                          {book.best_of === 'TRUE' && <Icon style={{color: '#c60000', marginRight: 5}} type="heart" theme="filled" />}
-                          {book.worst_of === 'TRUE' && <Icon style={{color: 'gray', marginRight: 5}} type="dislike" theme="filled" />}
-                      </span>
-                      {book.title}
-                  </div>
-              }
-              description={
-                <>
-                  {/*<div>{book.series}</div>*/}
-                  <div>by {book.author}</div>
-                  <div className="Book-year">{book.year_published}</div>
-                </>
-              }
-          />
-        </Card>
+          <a href={book.goodreads_link} target="_blank">
+            <Card
+                size="small"
+                hoverable
+                cover={
+                  <img src={book.image} alt="Book cover"/>
+                }
+            >
+              <Card.Meta
+                  title={
+                      <div>
+                          <span className="Book-icon">
+                              {book.best_of === 'TRUE' && <Icon style={{color: '#c60000', marginRight: 5}} type="heart" theme="filled" />}
+                              {book.worst_of === 'TRUE' && <Icon style={{color: 'gray', marginRight: 5}} type="dislike" theme="filled" />}
+                          </span>
+                          {book.title}
+                      </div>
+                  }
+                  description={
+                    <>
+                      {/*<div>{book.series}</div>*/}
+                      <div>by {book.author}</div>
+                      <div className="Book-year">{book.year_published}</div>
+                    </>
+                  }
+              />
+            </Card>
+          </a>
       </div>
   );
 };
