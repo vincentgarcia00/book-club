5695444ac9303adbff39fb1c421e0013056fad28
mknabe <fuzzy.tree7311@fastmail.com>
2023-03-14 22:33:11 -0500

    Update cache script to fail when payment required

--- scripts/buildCache.js
+++ scripts/buildCache.js
@@ -1,18 +1,29 @@
-fs = require('fs');
+fs = require("fs");
 
-const baseUrl = 'https://sheet.best/api/sheets/aa1f111c-28d5-4803-bf7f-64a3f2295352';
+const baseUrl =
+  "https://sheet.best/api/sheets/aa1f111c-28d5-4803-bf7f-64a3f2295352";
 
 const fetchData = (url, file) => {
-    fetch(`${baseUrl}${url ?? ''}`)
-        .then(response => response.json())
-        .then(json => fs.writeFile(`public/cache/${file}`, JSON.stringify(json, null, 2), callback));
-}
+  fetch(`${baseUrl}${url ?? ""}`)
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error(response.status + " " + response.statusText);
+    })
+    .then((json) =>
+      fs.writeFile(
+        `public/cache/${file}`,
+        JSON.stringify(json, null, 2),
+        callback
+      )
+    )
+    .catch(callback);
+};
 
 const callback = (err) => {
-    if (err) return console.error(err);
-    console.log('Success');
-}
+  if (err) return console.error(err);
+  console.log("Success");
+};
 
-fetchData('', 'books.json');
-fetchData('/tabs/Book%20Stats', 'stats.json');
-fetchData('/tabs/Reader%20Stats', 'readerStats.json');
+fetchData("", "books.json");
+fetchData("/tabs/Book%20Stats", "stats.json");
+fetchData("/tabs/Reader%20Stats", "readerStats.json");
---
+++ scripts/doTheThing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+npm run build:cache
+echo "done"
+#git add public/
+#git commit -m "Update cache"
+#git push origin master
+#npm run deploy
